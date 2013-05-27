f07bfc214588aea3a0d87e62061a7f3cd97af328
John Else <john.else@citrix.com>
2013-05-27 04:25:22 +0100

    Implement album search
    Signed-off-by: John Else <john.else@citrix.com>

--- lib/spotify_search.ml
+++ lib/spotify_search.ml
@@ -1,1 +1,16 @@
+module C = Cohttp_lwt_unix
+open Cohttp_lwt_unix_io
+
+exception No_response
+
 let base_url = "http://ws.spotify.com/search/1/"
+
+let search_albums query =
+  let uri = Uri.of_string (Printf.sprintf "%salbum.json?q=%s" base_url query) in
+  C.Client.call ~chunked:false `GET uri
+  >>= (fun result ->
+    match result with
+    | Some (_, body) ->
+      C.Body.string_of_body body
+      >>= (fun data -> return (Spotify_search_j.album_search_of_string data))
+    | None -> Lwt.fail No_response)
