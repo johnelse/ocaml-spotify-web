101e9ca4d5414558c1628c92026e052f3d494e15
John Else <john.else@citrix.com>
2013-05-27 18:49:32 +0100

    Implement artist search
    Signed-off-by: John Else <john.else@citrix.com>

--- lib/spotify_search.ml
+++ lib/spotify_search.ml
@@ -14,3 +14,13 @@ let search_albums query =
       C.Body.string_of_body body
       >>= (fun data -> return (Spotify_search_j.album_search_of_string data))
     | None -> Lwt.fail No_response)
+
+let search_artists query =
+  let uri = Uri.of_string (Printf.sprintf "%sartist.json?q=%s" base_url query) in
+  C.Client.call ~chunked:false `GET uri
+  >>= (fun result ->
+    match result with
+    | Some (_, body) ->
+      C.Body.string_of_body body
+      >>= (fun data -> return (Spotify_search_j.artist_search_of_string data))
+    | None -> Lwt.fail No_response)
